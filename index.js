c805793cf6160c6ca9d7826e8036b38e3355e39a
Rayhan Arayilakath <rayhanadev@pm.me>
2021-04-03 20:02:02 -0500

    Add an API server

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const readline = require('readline').createInterface({
 });
 
 console.clear();
-console.log(colors.rainbow(`
+console.log(
+	colors.rainbow(`
 ______ ___________ _                           
 | ___ \\  ___| ___ \\ |                          
 | |_/ / |__ | |_/ / |     __ _ _ __   __ _ ___ 
@@ -20,7 +21,8 @@ ______ ___________ _
 \\_| \\_\\____/\\_|  \\_____/\\__,_|_| |_|\\__, |___/
                                       __/ |    
                                      |___/     
-`));
+`)
+);
 console.log(colors.italic('Get the amount of languages a Repl has!\n'));
 
 function objectSortByValue(obj) {
@@ -38,6 +40,54 @@ function objectSortByValue(obj) {
 	return sorted;
 }
 
+function replLangs(data) {
+	if (data.name == 'NotFoundError') {
+		return {
+			error: "It seems like this Repl doesn't exist."
+		};
+	}
+
+	if (data.fileNames.length === 0) {
+		return {
+			error: "It seems like this Repl doesn't have files."
+		};
+	}
+
+	let fileExtensions = data.fileNames.map(file => {
+		return file.split('.').pop();
+	});
+
+	let fileCount = {};
+
+	for (let i = fileExtensions.length - 1; i > -1; i--) {
+		if (!fileCount[fileExtensions[i]]) {
+			fileCount[fileExtensions[i]] = 1;
+		} else {
+			fileCount[fileExtensions[i]] += 1;
+		}
+	}
+
+	let finalData = [];
+	for (const [key, value] of Object.entries(fileCount)) {
+		if (files[key]) {
+			finalData.push({ name: files[key], count: value });
+		} else {
+			finalData.push({ name: key, count: value });
+		}
+	}
+
+	finalData.sort((a, b) => {
+		return a.count - b.count;
+	});
+
+	let result = {
+		totalLanguages: fileExtensions.length,
+		languageCounts: finalData
+	};
+
+	return result;
+}
+
 readline.question('Enter anything to get started! \n', enter => {
 	console.clear();
 	console.log(colors.rainbow('R E P L A N G S!'));
@@ -52,65 +102,53 @@ readline.question('Enter anything to get started! \n', enter => {
 					let repl = new replapi.Repl(username, slug);
 					let info = await repl.replRESTData();
 
-					if (info.name == 'NotFoundError') {
-						console.log("\nHmm, it seem's this Repl doesn't exist :/");
-						process.exit(0);
-					}
-
-					let fileExtensions = info.fileNames.map(file => {
-						return file.split('.')[1];
-					});
-
-					if (fileExtensions.length === 0) {
-						console.log('\nThis Repl looks empty :/');
-						process.exit(0);
-					}
-
-					let fileCount = {};
-
-					for (let i = fileExtensions.length - 1; i > -1; i--) {
-						if (!fileCount[fileExtensions[i]]) {
-							fileCount[fileExtensions[i]] = 1;
-						} else {
-							fileCount[fileExtensions[i]] += 1;
-						}
-					}
-
-					let finalData = [];
-					for (const [key, value] of Object.entries(fileCount)) {
-						if (files[key]) {
-							finalData.push({ name: files[key], count: value });
-						} else {
-							finalData.push({ name: key, count: value });
-						}
-					}
-
-					finalData.sort((a, b) => {
-						return a.count - b.count;
-					});
-
-					console.log("\nHere's the Data!");
-					console.log(
-						colors.underline(`\n\nSummary for "${slug}" by ${username}`)
-					);
-					for (let i = finalData.length - 1; i > -1; i--) {
-						const bar = new cliProgress.SingleBar(
-							{
-								format: '{bar} {percentage}%'
-							},
-							cliProgress.Presets.shades_grey
+					let data = replLangs(info);
+					try {
+					  let totalLangs = data.languageCounts.length;
+						console.log("\nHere's the Data!");
+						console.log(
+							colors.underline(`\n\nSummary for "${slug}" by ${username}`)
 						);
-						console.log(`${finalData[i].name.toUpperCase()}: `);
-						bar.start(
-							100,
-							((finalData[i].count / fileExtensions.length) * 100).toFixed(1)
-						);
-						bar.stop();
+						for (let i = totalLangs - 1; i > -1; i--) {
+							const bar = new cliProgress.SingleBar(
+								{
+									format: '{bar} {percentage}%'
+								},
+								cliProgress.Presets.shades_grey
+							);
+							console.log(`${data.languageCounts[i].name.toUpperCase()}: `);
+							bar.start(
+								100,
+								(
+									(data.languageCounts[i].count / data.totalLanguages) *
+									100
+								).toFixed(1)
+							);
+							bar.stop();
+						}
+					} catch (err) {
+						console.log('\n\n\n' + data.error);
 					}
-					console.log("\n\n\n\nThanks for using REPLangs!");
+					console.log('\n\n\n\nThanks for using REPLangs!');
 					readline.close();
 				}
 			);
 		}
 	);
 });
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.end('Send Get Requests to `/[username]/[slug]`');
+});
+
+app.get('/:username/:slug', async (req, res) => {
+	let repl = new replapi.Repl(req.params.username, req.params.slug);
+	let info = await repl.replRESTData();
+
+	res.end(JSON.stringify(replLangs(info)));
+});
+
+app.listen(3000);
